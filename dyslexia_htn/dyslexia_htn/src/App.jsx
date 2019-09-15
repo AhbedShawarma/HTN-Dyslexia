de1e9a1eb191639fa4ceb8a87b3312bc1758865c
Abhay Sharma <45717469+AhbedShawarma@users.noreply.github.com>
2019-09-15 03:59:48 -0400

    front end

---
+++ dyslexia_htn/dyslexia_htn/src/App.jsx
@@ -0,0 +1,21 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Blank from "./components/Blank";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="AppHeader">
+          <a>workn</a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
